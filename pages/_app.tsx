200dc8f0cf35793aafc9a5b5e8bd347268f6529a
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-15 16:20:09 +0300

    feat: added Themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import MainTheme from '../components/Layouts/MainTheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   console.log('Component', Component)
   console.log('pageProps', pageProps)
-  return <Component {...pageProps} />
+  return (
+    <MainTheme>
+      <Component {...pageProps} />
+    </MainTheme>
+  )
 }
 
 export default MyApp
