fc22856955c3832bce2bb42016d0ae8423825992
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-16 17:39:58 +0300

    feat:added helper for taking parentPath

---
+++ helpers/index.ts
@@ -0,0 +1,9 @@
+export const ParentPath = (path: string, query: string | undefined): string => {
+  if (query !== undefined) {
+    const lengthOfQuery = query.length
+    const parentPath = path.slice(0, -lengthOfQuery)
+    return parentPath
+  } else {
+    return ''
+  }
+}
