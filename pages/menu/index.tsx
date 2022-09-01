5909f2f8cb9ee37abc833014e40591c8eff730f2
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-09-01 17:02:45 +0300

    refactor: changed type of event

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next'
-import { ReactEventHandler, useState } from 'react'
+import { SyntheticEvent, useState } from 'react'
 import ContainerBig from '../../components/Layouts/ContainerBig'
 import Product from '../../components/Product'
 import { DEFAULT_CATEGORY } from '../../constants'
@@ -12,40 +12,26 @@ const Menu: NextPage<any> = ({ sandwiches }: { sandwiches: Array<any> }) => {
   const [products, setProducts] = useState<Array<any>>(sandwiches)
 
   const btnHandler = async () => {}
-  const selectHander = (e: ReactEventHandler<HTMLSelectElement>) => {}
+  const selectHander = (e: SyntheticEvent<HTMLSelectElement, Event>) => {}
 
   return (
     <ContainerBig>
       <div className={styles.menu}>
         <h2>Menu Page</h2>
         <span className={styles.menu__item}>Select the category:</span>
-        <select
-          defaultValue={DEFAULT_CATEGORY}
-          onSelect={selectHander}
-          className={`${styles.menu__select} ${styles.menu__item}`}
-        >
+        <select defaultValue={DEFAULT_CATEGORY} onSelect={selectHander} className={`${styles.menu__select} ${styles.menu__item}`}>
           <option value='sandwiches'>Sandwiches</option>
           <option value='pizza'>Pizza</option>
           <option value='burgers'>Burgers</option>
           <option value='salads'>Salads</option>
           <option value='drinks'>Drinks</option>
         </select>
-        <button
-          onClick={btnHandler}
-          className={`${styles.menu__btn} ${styles.menu__item}`}
-        >
+        <button onClick={btnHandler} className={`${styles.menu__btn} ${styles.menu__item}`}>
           Show
         </button>
         <div className={styles.content}>
           {products.map((el) => {
-            return (
-              <Product
-                key={el.id}
-                name={el.name}
-                price={el.price}
-                description={el.description}
-              ></Product>
-            )
+            return <Product key={el.id} name={el.name} price={el.price} description={el.description}></Product>
           })}
         </div>
       </div>
