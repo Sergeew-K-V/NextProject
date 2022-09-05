e2a1d1fc459ed44670afc66ec0f06c557b8e1578
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-09-05 17:36:49 +0300

    refactor: closing menu on link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,30 +4,32 @@ import styles from '../scss/Nav.module.scss'
 import NavProps from '../types/NavProps'
 
 const Nav: FC<NavProps> = ({ isOpen, setIsOpen }: NavProps) => {
+  const CloseMenu = () => setIsOpen(false)
+
   return (
     <div className={styles.navigation}>
       <div className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
-        <div className={styles.nav__item}>
+        <div className={styles.nav__item} onClick={CloseMenu}>
           <Link href='/'>
             <a>Home</a>
           </Link>
         </div>
-        <div className={styles.nav__item}>
+        <div className={styles.nav__item} onClick={CloseMenu}>
           <Link href='/about'>
             <a>About</a>
           </Link>
         </div>
-        <div className={styles.nav__item}>
+        <div className={styles.nav__item} onClick={CloseMenu}>
           <Link href='/posts'>
             <a>Posts</a>
           </Link>
         </div>
-        <div className={styles.nav__item}>
+        <div className={styles.nav__item} onClick={CloseMenu}>
           <Link href='/menu'>
             <a>Menu</a>
           </Link>
         </div>
-        <div className={styles.nav__item}>
+        <div className={styles.nav__item} onClick={CloseMenu}>
           <Link href='/crypto'>
             <a>CRYPTO</a>
           </Link>
