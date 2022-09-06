d7d5f47a7d19bb9c4fafc0ba0e7681ff449d3969
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-09-06 12:54:23 +0300

    feat: added crypto page and fix nav index

--- pages/crypto/Crypto.tsx
+++ pages/crypto/Crypto.tsx
@@ -1,9 +1,18 @@
 import { NextPage } from 'next'
 import { PropsWithChildren } from 'react'
+import { ContainerBig } from '../../components/Layouts'
+import styles from '../../scss/Crypto.module.scss'
 
-const Crypto: NextPage<PropsWithChildren> = () =>{
-    return <div></div>
+const Crypto: NextPage<PropsWithChildren> = () => {
+  return (
+    <ContainerBig>
+      <div className={styles.content}>
+        <h2> Here you can mint token!</h2>
+        <div className={styles.descrition}>You`ll be owner of unique token. It`ll be placed in one of multiple blockchains.</div>
+        <button className={styles.btn}>Mint token</button>
+      </div>
+    </ContainerBig>
+  )
 }
-
 
 export default Crypto
