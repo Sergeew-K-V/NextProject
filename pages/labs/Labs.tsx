12d58fa5b397a3f065b4768a3680b52aa7122db0
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-10-10 17:47:27 +0300

    feat: get data from cluster

--- pages/labs/Labs.tsx
+++ pages/labs/Labs.tsx
@@ -1,16 +1,20 @@
 import { NextPage } from 'next'
 import { ContainerBig } from '../../components/Layouts'
-import styled from 'styled-components'
 import { LabsProps } from '../../types/Labs'
 import { useFetch } from '../../hooks/useFetch'
 import { URL_LABS } from '../../constants/URLS'
+import { useState } from 'react'
+import styled from 'styled-components'
 
 const Labs: NextPage<LabsProps> = () => {
   const { request } = useFetch()
+  const [requestLimit, setRequestLimit] = useState<number>(0)
+  const [weatherData, setWeatherData] = useState<any[] | null>(null)
 
   const GeneratorHandlerWeatherData = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
-    const data = await request(`${URL_LABS}/weather?_limit=1000`)
+    const data = await request(`${URL_LABS}/weather?_limit=${requestLimit}`)
+    setWeatherData(data)
     console.log(data)
   }
 
@@ -21,14 +25,24 @@ const Labs: NextPage<LabsProps> = () => {
         <Flex>
           <Form>
             <Title>Go to fetch</Title>
+            <Input type='number' max={10000} min={0} value={requestLimit} onChange={(e) => setRequestLimit(Number(e.target.value))} />
             <Flex>
               <Button type='submit' onClick={GeneratorHandlerWeatherData}>
-                FETCH
+                Download weather data
               </Button>
             </Flex>
           </Form>
         </Flex>
       </Controlers>
+      <span>
+        {weatherData
+          ? weatherData.map((data, index) => (
+              <div key={index}>
+                {JSON.stringify(data)} <br />
+              </div>
+            ))
+          : "Weather data wasn't download"}
+      </span>
     </ContainerBig>
   )
 }
