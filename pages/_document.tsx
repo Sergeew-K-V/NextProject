0dbd5f4836d63c711dab570f11d95d310ed9ea38
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-10-17 15:52:56 +0300

    feat: added new fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link href='https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap' rel='stylesheet' />
       </Head>
       <body>
         <Main />
