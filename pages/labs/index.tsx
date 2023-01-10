f3fec432c3bae956d64f1dee9f144a338b8bd80f
Sergeew Kirill <kirill.sergeev@progforce.com>
2023-01-10 16:37:55 +0300

    urls fix

--- pages/labs/index.tsx
+++ pages/labs/index.tsx
@@ -4,8 +4,7 @@ import { QueryRangesLogic } from "../../utils"
 export { default } from "./Labs"
 
 export async function getStaticProps() {
-  const url = `${URL_LABS_SERVER}/weather?${QueryRangesLogic(0, 0)}`
-  const res = await fetch(url)
+  const res = await fetch(`${URL_LABS_SERVER}` + `/weather?${QueryRangesLogic(0, 0)}`)
   const preloadWeatherData = await res.json()
   return {
     props: {
